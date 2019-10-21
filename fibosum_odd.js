0543047931da6dd5b869471abf05c5727c5ad176
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2019-10-21 19:15:18 -0400

    WIP: sum odd fibonacci numbers in a range

---
+++ fibosum_odd.js
@@ -0,0 +1,17 @@
+function sumFibs(num) {
+    if(num<3){
+      return 1;
+    }
+    var fib;
+    for(var i=0; i< num; i++){
+      if(i%2==0){
+        
+      }
+    }
+    return num;
+  }
+  
+  sumFibs(4);
+
+// Time = 22
+// Status = incomplete
