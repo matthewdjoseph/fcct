43946ba999ed47a3a0c5c02fdf5b84a733eea0a4
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2019-10-28 18:55:02 -0400

    convert to spinal case

---
+++ spinalcase.js
@@ -0,0 +1,9 @@
+function spinalCase(str) {
+  
+    return str.replace(/(?!^)([A-Z])/g, ' $1')
+              .replace(/[_\s]+(?=[a-zA-Z])/g, '-').toLowerCase();
+  }
+  
+  spinalCase('This Is Spinal Tap');
+
+  // Time = 4
