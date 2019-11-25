6967ae6b28ec02b071790234a53205072207e23c
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2019-11-25 18:41:25 -0500

    diff two arrays

---
+++ diffarrays.js
@@ -0,0 +1,10 @@
+function diffArray(arr1, arr2) {
+  let newArr = arr1.filter(x => !arr2.includes(x))
+              .concat(arr2.filter(x => !arr1.includes(x)));
+  
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+// Time = 4.5
